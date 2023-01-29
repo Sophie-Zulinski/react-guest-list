db7a56f8d0d6e1044ff185c035925e8c2f629c59
Sophie-Zulinski <sophie.zulinski@gmx.at>
2023-01-29 12:15:20 +0100

    Wrong version, just to save it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { useState } from 'react';
 
+let nextId = 1;
+
 export default function Form() {
-  const [guest, setGuest] = useState({
-    firstName: '',
-    lastName: '',
-    attending: false,
-  });
+  const [guest, setGuest] = useState([
+    {
+      firstName: '',
+      lastName: '',
+      attending: false,
+    },
+  ]);
 
   function handlefirstName(e) {
     setGuest({
@@ -31,11 +35,25 @@ export default function Form() {
     });
   }
 
-  console.log(guest);
+  function handleClick() {
+    const insertAt = 1; // Could be any index
+    const nextArtists = [
+      // Items before the insertion point:
+      ...guest.slice(0, insertAt),
+      // New item:
+      { id: nextId++, ...guest },
+      // Items after the insertion point:
+      ...guest.slice(insertAt),
+    ];
+    setGuest(nextArtists);
+    setGuest('');
+  }
+
+  console.log('guest 01', guest);
 
   return (
     <>
-      <form onSubmit={handleAttending}>
+      <form>
         <label>
           FirstName
           <input value={guest.firstName} onChange={handlefirstName} />
@@ -44,12 +62,23 @@ export default function Form() {
           LastName
           <input value={guest.lastName} onChange={handlelastName} />
         </label>
+        {/*
         <input
           checked={guest.attending}
           type="checkbox"
           onChange={handleAttending}
         />
         <br />
+        <br />
+        <br />
+        {/* key
+        <button onClick={handleClick}>Insert</button>
+        <ul>
+          {guest.map((artist) => (
+            <li key={nextId++}>{guest.lastName}</li>
+          ))}
+        </ul>*/}
+        {console.log('guest 02', guest)}
 
         <button onClick={() => useState}>Remove</button>
       </form>
